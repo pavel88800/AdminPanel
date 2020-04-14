0cf08e8650f9815da3a81aa269ee64e0b16a8a94
Огородников П.С <OgorodnikovPS@tomskasu.ru>
2020-04-14 09:43:40 +0700

    добавил middleware для обработки ошибок

---
+++ APProject/APProject/Middleware/AppMiddlewareExtension.cs
@@ -0,0 +1,18 @@
+﻿using APP.Common.Middlewares;
+using Microsoft.AspNetCore.Builder;
+
+namespace APProject.Middleware
+{
+    /// <summary>
+    ///     Экстеншны для использования middleware.
+    /// </summary>
+    public static class AppMiddlewareExtension
+    {
+        /// <summary>
+        ///     Регистрация middleware для обработки ошибок при выполнении запроса.
+        /// </summary>
+        /// <param name="builder"> Билдер приложения. </param>
+        public static IApplicationBuilder UseHttpStatusCodeExceptionMiddleware(this IApplicationBuilder builder) =>
+            builder.UseMiddleware<ExceptionMiddleware>();
+    }
+}
