d17576d6bf5799d2f9508597da0e8e73fbfbfdfc
Pavel Ogorodnikov <pavel88800@yandex.ru>
2020-05-16 15:54:16 +0700

    Добавил отправку писем

---
+++ APProject/APProject/Controllers/Api/SendEmailController.cs
@@ -0,0 +1,38 @@
+﻿namespace APProject.Controllers.Api
+{
+    using APP.Notification.Interfaces;
+    using APProject.Controllers.Base;
+    using Microsoft.AspNetCore.Mvc;
+
+    /// <summary>
+    ///     Контроллер по отправке email писем.
+    /// </summary>
+    public class SendEmailController : BaseApiController
+    {
+        /// <summary>
+        ///     Сервис по отправке писем
+        /// </summary>
+        public IEmailNotificationService _notification;
+
+        /// <summary>
+        ///     Конструктор.
+        /// </summary>
+        /// <param name="notification">сервис</param>
+        public SendEmailController(IEmailNotificationService notification)
+        {
+            _notification = notification;
+        }
+
+        /// <summary>
+        ///     Отправить письмо.
+        /// </summary>
+        /// <param name="email">адрес получателя</param>
+        /// <param name="subject">тема письма</param>
+        /// <param name="message">содержимое письма</param>
+        [HttpPost]
+        public void Send(string email, string subject, string message)
+        {
+            _notification.SendEmailAsync(email, subject, message);
+        }
+    }
+}
---
+++ Common/APP.Notification/Services/EmailNotificationService.cs
@@ -0,0 +1,38 @@
+﻿namespace APP.Notification.Services
+{
+    using System.Threading.Tasks;
+    using APP.Notification.Interfaces;
+    using MailKit.Net.Smtp;
+    using MimeKit;
+    using MimeKit.Text;
+
+    /// <summary>
+    ///     Сервис по отправке писем на e-mail
+    /// </summary>
+    public class EmailNotificationService : IEmailNotificationService
+    {
+        /// <inheritdoc />
+        public async Task SendEmailAsync(string email, string subject, string message)
+        {
+            var emailMessage = new MimeMessage();
+
+            emailMessage.From.Add(new MailboxAddress("Администрация сайта", "pavel88800@yandex.ru"));
+            emailMessage.To.Add(new MailboxAddress("", "pavel88800@yandex.ru"));
+            emailMessage.Subject = subject;
+            emailMessage.Body = new TextPart(TextFormat.Html)
+            {
+                Text =
+                    $"<!DOCTYPE html ><html xmlns=\"\"><head> <meta http-equiv=\"Content-Type\" content=\"text/html; charset=utf-8\" /> <meta name=\"viewport\" content=\"width=device-width\"/> <!-- For development, pass document through inliner --> <link rel=\"stylesheet\" href=\"css/simple.css\"> <style type=\"text/css\"> /* Your custom styles go here */ </style></head><body><table class=\"body-wrap\"> <tr> <td class=\"container\"> <!-- Message start --> <table> <tr> <td align=\"center\" class=\"masthead\">  </td> </tr> <tr> <td class=\"content\"> <h2>Здравствуйте!</h2> <p>{message}</p> <table> <tr> <td align=\"center\"> <p> <a href=\"#\" class=\"button\">Share the Awesomeness</a> </p> </td> </tr> </table>  <p><em>– Mr. Pen</em></p> </td> </tr> </table> </td> </tr> <tr> <td class=\"container\"> <!-- Message start --> <table> <tr> <td class=\"content footer\" align=\"center\">  </td> </tr> </table> </td> </tr></table></body></html>"
+            };
+
+            using (var client = new SmtpClient())
+            {
+                await client.ConnectAsync("smtp.yandex.ru", 25, false);
+                await client.AuthenticateAsync("pavel88800@yandex.ru", "es15102006es");
+                await client.SendAsync(emailMessage);
+
+                await client.DisconnectAsync(true);
+            }
+        }
+    }
+}
